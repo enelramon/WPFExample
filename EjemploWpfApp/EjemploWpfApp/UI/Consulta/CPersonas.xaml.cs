dd9a67232de345f48b8830683810dd2ac9b46ff2
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-02-11 20:32:02 -0400

    Add files via upload

---
+++ EjemploWpfApp/EjemploWpfApp/UI/Consulta/CPersonas.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using EjemploWpfApp.BLL;
+using EjemploWpfApp.Entidades;
+
+namespace EjemploWpfApp.UI.Consulta
+{
+    /// <summary>
+    /// Interaction logic for CPersonas.xaml
+    /// </summary>
+    public partial class CPersonas : Window
+    {
+        public CPersonas()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Listado = new List<Persona>();
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    //Todo
+                    case 0:
+                        Listado = PersonasBLL.GetList(p => true);
+                        break;
+                    //ID
+                    case 1:
+                        int id = Convert.ToInt32(CriterioTextBox.Text);
+                        Listado = PersonasBLL.GetList(p => p.PersonaId == id);
+                        break;
+                    //Nombre
+                    case 2:
+                        Listado = PersonasBLL.GetList(p => p.Nombres.Contains(CriterioTextBox.Text));
+                        break;
+                    //Cedula
+                    case 3:
+                        Listado = PersonasBLL.GetList(p => p.Cedula.Contains(CriterioTextBox.Text));
+                        break;
+                    //Direccion
+                    case 4:
+                        Listado = PersonasBLL.GetList(p => p.Direccion.Contains(CriterioTextBox.Text));
+                        break;
+                }
+
+                Listado = Listado.Where(c => c.FechaNacimiento.Date >= DesdeDatePicker.SelectedDate && c.FechaNacimiento.Date <= HastaDatePicker.SelectedDate).ToList();
+            }
+            else
+            {
+                Listado = PersonasBLL.GetList(p => true);
+            }
+
+            ConsultaDataGrid.ItemsSource = null;
+            ConsultaDataGrid.ItemsSource = Listado;
+        }
+    }
+}
---
+++ EjemploWpfApp/EjemploWpfApp/UI/Registro/RPersonas.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using EjemploWpfApp.Entidades;
+using EjemploWpfApp.BLL;
+using EjemploWpfApp.UI.Consulta;
+
+namespace EjemploWpfApp.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for RPersonas.xaml
+    /// </summary>
+    public partial class RPersonas : Window
+    {
+        public RPersonas()
+        {
+            InitializeComponent();
+            PersonaIdTextBox.Text = "0";
+            FechaNacimientoDatePicker.SelectedDate = DateTime.Now;
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            CPersonas cp = new CPersonas();
+            cp.Show();
+        }
+
+        private void Limpiar()
+        {
+            PersonaIdTextBox.Text = "0";
+            NombresTextBox.Text = string.Empty;
+            TelefonoTextBox.Text = string.Empty;
+            CedulaTextBox.Text = string.Empty;
+            DireccionTextBox.Text = string.Empty;
+            FechaNacimientoDatePicker.SelectedDate = DateTime.Now;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private Persona LlenaClase()
+        {
+            Persona persona = new Persona();
+
+            persona.PersonaId = Convert.ToInt32(PersonaIdTextBox.Text);
+            persona.Nombres = NombresTextBox.Text;
+            persona.Telefono = TelefonoTextBox.Text;
+            persona.Cedula = CedulaTextBox.Text;
+            persona.Direccion = DireccionTextBox.Text;
+            persona.FechaNacimiento = Convert.ToDateTime(FechaNacimientoDatePicker.SelectedDate);
+
+            return persona;
+        }
+
+        private void LlenaCampo(Persona persona)
+        {
+            PersonaIdTextBox.Text = Convert.ToString(persona.PersonaId);
+            NombresTextBox.Text = persona.Nombres;
+            TelefonoTextBox.Text = persona.Telefono;
+            CedulaTextBox.Text = persona.Cedula;
+            DireccionTextBox.Text = persona.Direccion;
+            FechaNacimientoDatePicker.SelectedDate = persona.FechaNacimiento;
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+
+            if(string.IsNullOrWhiteSpace(NombresTextBox.Text))
+            {
+                MessageBox.Show("El Campo Nombres no puede estar Vacío");
+                NombresTextBox.Focus();
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(TelefonoTextBox.Text))
+            {
+                MessageBox.Show("El Campo Telefono no puede estar Vacío");
+                TelefonoTextBox.Focus();
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(CedulaTextBox.Text.Replace("-", "")))
+            {
+                MessageBox.Show("El Campo Cedula no puede estar Vacío");
+                CedulaTextBox.Focus();
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(DireccionTextBox.Text))
+            {
+                MessageBox.Show("El Campo Dirección no puede estar Vacío");
+                DireccionTextBox.Focus();
+                paso = false;
+            }
+
+            return paso;
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Persona persona = PersonasBLL.Buscar(Convert.ToInt32(PersonaIdTextBox.Text));
+
+            return (persona != null);
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Persona persona = new Persona();
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            persona = LlenaClase();
+
+            //determinar si es guardar o modificar
+            if (PersonaIdTextBox.Text == "0")
+                paso = PersonasBLL.Guardar(persona);
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede Modificar una persona que no existe");
+                    return;
+                }
+                paso = PersonasBLL.Modificar(persona);
+            }
+
+            //informar resultado
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado");
+            }
+            else
+                MessageBox.Show("No se pudo Guardar");
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            int id;
+            int.TryParse(PersonaIdTextBox.Text, out id);
+
+            Limpiar();
+
+            if (PersonasBLL.Eliminar(id))
+            {
+                MessageBox.Show("Eliminado Correctamente");
+            }
+            else
+                MessageBox.Show("No se Puede Eliminar una perona que no existe");
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            int id;
+            Persona persona = new Persona();
+            int.TryParse(PersonaIdTextBox.Text, out id);
+
+            Limpiar();
+
+            persona = PersonasBLL.Buscar(id);
+
+            if (persona != null)
+            {
+                MessageBox.Show("Encontrado");
+                LlenaCampo(persona);
+            }
+            else
+                MessageBox.Show("No existe");
+        }
+    }
+}
